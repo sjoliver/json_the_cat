12b291296a5dc1ec033103abe4c15644b360ea3f
Sophie <sophiejoliver19+1@gmail.com>
2021-09-24 00:12:21 +0000

    add unit tests for breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,26 +1,26 @@
 const request = require('request');
 
-const args = process.argv.slice(2); // command line arguments
-const breed = args[0]; // breed input from CLI
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
   
-  if (error) {
-    console.log('error:', error); // logs error if present
-    return;
-  }
+    const data = JSON.parse(body); // parse body string object into an object
+  
+    if (data.length === 0) {
+      callback(`${breedName} does not exist. Please search again.`, null); // edge case: breed not found/invlaid breed input
+      return;
+    }
 
-  // parse body string object into an object
-  const data = JSON.parse(body);
+    const description = data[0].description;
+    callback(null, description);
+  
+  });
+};
 
-  // edge case: breed not found/invlaid breed input
-  if (data.length === 0) {
-    console.log(`${breed} does not exist. Please search again.`);
-    return;
-  }
-
-  const description = data[0].description;
-  console.log("Description:", description);
-
-});
-
+module.exports = {
+  fetchBreedDescription
+};
